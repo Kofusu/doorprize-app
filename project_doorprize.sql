a4b03c43083dab65f7b12999e53de13a95cab029
Hendratara Pratama <Kofusu>
2023-03-28 13:10:24 +0700

    feat: addding backup database

--- project_doorprize.sql
+++ project_doorprize.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 03, 2023 at 07:11 PM
+-- Generation Time: Mar 28, 2023 at 01:03 PM
 -- Server version: 8.0.32-0ubuntu0.20.04.2
 -- PHP Version: 7.4.3-4ubuntu2.17
 
@@ -32,15 +32,14 @@ CREATE TABLE `auth` (
   `id_auth` int NOT NULL,
   `username` varchar(20) DEFAULT NULL,
   `password` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `auth`
 --
 
 INSERT INTO `auth` (`id_auth`, `username`, `password`) VALUES
-(1, 'admin', 'doorprize098'),
-(2, 'admin', 'adminadmin');
+(1, 'admin', 'doorprize098');
 
 -- --------------------------------------------------------
 
@@ -51,20 +50,10 @@ INSERT INTO `auth` (`id_auth`, `username`, `password`) VALUES
 CREATE TABLE `prize` (
   `id_prize` int NOT NULL,
   `nama_prize` varchar(32) DEFAULT NULL,
-  `nama_gambar` varchar(252) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nama_gambar` varchar(252) CHARACTER SET utf8mb4 DEFAULT NULL,
   `id_session` int DEFAULT NULL,
   `max_winner` int DEFAULT '5'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `prize`
---
-
-INSERT INTO `prize` (`id_prize`, `nama_prize`, `nama_gambar`, `id_session`, `max_winner`) VALUES
-(1, 'Sepeda JBL', 'SepedaJBL.png', 1, 5),
-(40, 'ff', '1677768519577.png', 1, 4),
-(41, 'erf', '1677769540418.png', 1, 4),
-(42, 'SS an', '1677772258726.png', 1, 45);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -75,19 +64,7 @@ INSERT INTO `prize` (`id_prize`, `nama_prize`, `nama_gambar`, `id_session`, `max
 CREATE TABLE `sessions` (
   `id_session` int NOT NULL,
   `nama_session` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `sessions`
---
-
-INSERT INTO `sessions` (`id_session`, `nama_session`) VALUES
-(1, 'sesi 1'),
-(29, 'Sesi 2'),
-(30, 'Sesi Utama'),
-(31, 'Sesi Utama 2'),
-(32, 'Sesi 5'),
-(33, 'Sesi 6');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -100,18 +77,13 @@ CREATE TABLE `users` (
   `nama_user` varchar(30) DEFAULT NULL,
   `no_hp` varchar(18) DEFAULT NULL,
   `domisili` varchar(30) DEFAULT NULL,
-  `email` varchar(32) DEFAULT NULL,
+  `nama_pt` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL,
   `status` enum('active','blacklist') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
-
-INSERT INTO `users` (`id_user`, `nama_user`, `no_hp`, `domisili`, `email`, `status`) VALUES
-(1, 'Faizah Masturina Yuatno', '087851931307', 'depok', 'ijah@gmail.com', 'active'),
-(2, 'Hendratara pratama', '087851930605', 'depok', 'tara@gmail.com', 'blacklist'),
-(3, 'Oryza', '087851932323', 'depok', 'oyii@gmail.com', 'active');
 
 -- --------------------------------------------------------
 
@@ -123,14 +95,7 @@ CREATE TABLE `winner` (
   `id_winner` int NOT NULL,
   `id_user` int DEFAULT NULL,
   `id_prize` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `winner`
---
-
-INSERT INTO `winner` (`id_winner`, `id_user`, `id_prize`) VALUES
-(1, 2, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -177,31 +142,31 @@ ALTER TABLE `winner`
 -- AUTO_INCREMENT for table `auth`
 --
 ALTER TABLE `auth`
-  MODIFY `id_auth` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_auth` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `prize`
 --
 ALTER TABLE `prize`
-  MODIFY `id_prize` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id_prize` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `id_session` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_session` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `winner`
 --
 ALTER TABLE `winner`
-  MODIFY `id_winner` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_winner` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
